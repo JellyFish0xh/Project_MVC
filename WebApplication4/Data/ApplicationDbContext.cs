f79f11b557f0a50f4004a9109e4862038468efed
يوسف محمود فوزى ابراهيم مراد <ymorad@std.mans.edu.eg>
2023-11-27 02:06:25 +0200

    Add project files.

---
+++ WebApplication4/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using WebApplication4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<FakeTable> FakeTable {  get; set; }
+
+    }
+}
